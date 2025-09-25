4df9905bdd71f3146c471caa8a7a1647b94d18d8
♱ 𝙈𝙞𝙠𝙚𝙮𝙯¡! 🎭• 斬 <lemarquis3944@gmail.com>
2025-09-25 13:02:44 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+// service-worker.js - Mettre dans le dossier racine
+const CACHE_NAME = 'jwetlakay-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/main.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
